84a42ebd57eb00f576e3dc8321490707a01a9b9d
cyrusc2026 <cyrusc2026@student.cis.edu.hk>
2024-02-05 21:26:04 +0800

    Version 1.1

--- src/Dateable.java
+++ src/Dateable.java
@@ -1,6 +1,14 @@
+import java.util.Scanner;
+
 public class Dateable {
 
     public static void main(String[] args) {
-        
+    Scanner scanner = new Scanner(System.in);
+    System.out.println("Enter your age:");
+            int age = scanner.nextInt();
+                    int youngestage = age/2+7;
+        System.out.println(age + "-year olds should date somebody who is at least " + youngestage + " years old.");
+
+
     }
 }
